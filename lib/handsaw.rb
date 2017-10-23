698d0128dd5b832369fdc2eab2612ad9ae112ecf
sho ishihara <sho.ishihara@theport.jp>
2017-10-23 17:49:51 +0900

    add code_block filter

--- lib/handsaw.rb
+++ lib/handsaw.rb
@@ -3,6 +3,7 @@ require 'handsaw/markdown'
 
 require 'handsaw/filters/br_parser'
 require 'handsaw/filters/doc_parser'
+require 'handsaw/filters/code_block'
 require 'handsaw/filters/indented_paragraph'
 require 'handsaw/filters/link_parser'
 require 'handsaw/filters/sanitizer'
---
+++ lib/handsaw/filters/code_block.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+module Handsaw
+  module Filters
+    class CodeBlock < HTML::Pipeline::Filter
+      include ActionView::Helpers
+      include HtmlBuilder
+
+      def element_type
+        self.class.to_s.split('::').last.underscore
+      end
+
+      def call
+        variables = instance_variables
+        doc.search('pre').each do |pre|
+          next unless (codes = pre.at('code'))
+          next unless (@type = codes.attributes.first&.last&.value) == element_type
+          @value = pre.text
+          pre.replace Nokogiri::HTML.fragment(template)
+        end
+        (instance_variables - variables).each { |v| remove_instance_variable v }
+        doc
+      end
+
+      def template
+        raise NotImplementedError
+      end
+    end
+  end
+end
